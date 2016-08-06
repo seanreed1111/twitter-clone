303a2e1c74ee02a2dc7f9fa467c781fd1c40e9b3
seanreed1111 <seanreed1111@gmail.com>
2016-08-06 12:16:22 -0400

    adds ffaker gem and uses it to create new users and seed tweets. seed is now commented out. seed can be run from console using the command 'Rails.application.load_seed'

--- app/helpers/data_helper.rb
+++ app/helpers/data_helper.rb
@@ -1,3 +1,9 @@
 module DataHelper
-  
+  def tweet_builder
+  end
+
+  private
+
+  def data_faker
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#new_users = []
+
+# for i in 0..5
+#   new_users << {
+#     name: FFaker::Name.name, 
+#     email: FFaker::Internet.safe_email, 
+#     password: 'password',
+#     password_confirmation: 'password'
+#   }
+# end
+
+# new_users.each do |new_user|
+#   User.create(new_user)
+# end
+
+# totalUsers = User.all
+
+# totalUsers.each do |user|
+#   for j in 1..rand(1..14)
+#     user.microposts.create(title: FFaker::Movie.title, content: FFaker::Lorem.phrase)
+#   end
+# end
